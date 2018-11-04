97235f89988dcac300bf33ae752545837bf589b0
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2018-11-04 23:53:50 +0200

    Test.with_category should be class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many   :marks
 
-  def with_category(title)
+  def self.with_category(title)
     Test.all.where(
         category: Category.where(title: title)
     )
