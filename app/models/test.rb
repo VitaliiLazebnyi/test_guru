41e65b84b96f295ff6db3d79ed8db6fae8d35e69
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2018-11-04 19:47:07 +0200

    Task 4. Associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,12 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many   :marks
+
+  def self.with_category(title)
+    Test.select(:title)
+        .joins('JOIN categories ON tests.category_id = categories.id')
+        .where('categories.title': title)
+        .order(title: :desc)
+        .pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user     = User.create(name: 'Vitalii')
+category = Category.create(title: 'General')
+test     = Test.create(title: 'Rails', category: category)
+question = Question.create(body: 'What is Rails?', test: test)
+answer   = Answer.create(body: 'Fullstack rfamework.', question: question)
+mark     = Mark.create(value: 5, user: user, test: test)
