afc25e028fb7327604e94850f88b3f91516bbd06
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2018-11-10 13:37:33 +0200

    change string to symbol

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :marks
   has_many :passed_tests, through: :marks, source: :test
-  has_many :owned_tests, class_name: :Test, foreign_key: 'author_id'
+  has_many :owned_tests, class_name: :Test, foreign_key: :author_id
 
   def passed_with(level)
     Test.where(level: level, 'marks.user_id': id)
