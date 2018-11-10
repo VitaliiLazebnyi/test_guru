3cd12d1f5259fddb342176a400c5e2894c121dca
Vitaliy Lazebny <VitaliyLazebny@users.noreply.github.com>
2018-11-10 13:42:37 +0200

    corrected many-to-many associations (#4)
    corrected many-to-many associations and others db restrictions

---
+++ db/migrate/20181106221058_tests_author.rb
@@ -0,0 +1,5 @@
+class TestsAuthor < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :tests, :author
+  end
+end
---
+++ db/migrate/20181109095134_add_constrains.rb
@@ -0,0 +1,10 @@
+class AddConstrains < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null(:answers,   :question_id, false)
+    change_column_null(:marks,     :user_id,     false)
+    change_column_null(:marks,     :test_id,     false)
+    change_column_null(:questions, :test_id,     false)
+    change_column_null(:tests,     :category_id, false)
+    change_column_null(:tests,     :author_id,   false)
+  end
+end
