8fca9975dba27327a485b82a6d2400968adeaa7a
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2018-11-18 04:00:43 +0200

    Task 8. Views, Work with resources

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def question_header
+    action = @question.new_record? ? 'Create' : 'Edit'
+    "#{action} <#{@question.test.title}> Question"
+  end
+
+  def github_url(author, repo)
+    link_to author, "https://github.com/#{repo}", target: '_blank'
+  end
+
+  def current_year
+    Time.current.year
+  end
 end
