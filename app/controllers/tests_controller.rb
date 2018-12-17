3a112756a0816ddbe27d2aa5579a7279a4091eb3
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2018-12-17 11:20:58 +0200

    tmp

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,16 @@
+class TestsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_test, only: :show
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+end
