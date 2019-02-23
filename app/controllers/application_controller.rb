6f83f83c9b6497edfda724c324f88ecbec13bd1b
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2019-02-23 23:02:10 +0200

    Task 12. Internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    I18n.locale != I18n.default_locale ? { lang: I18n.locale } : {}
+  end
 
   protected
 
@@ -11,10 +16,12 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(_resource)
-    if current_user.admin?
-      admin_tests_path
-    else
-      tests_path
-    end
+    current_user.admin? ? admin_tests_path : tests_path
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ?
+                      params[:lang] :
+                      I18n.default_locale
   end
 end
