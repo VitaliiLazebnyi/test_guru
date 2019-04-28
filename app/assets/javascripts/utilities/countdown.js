2c9c871592c7db538880dccdf7abd5220c30c114
Vitalii Lazebnyi <vitaliy.lazebny@gmail.com>
2019-04-28 13:49:40 +0300

    refactoring of counter js code

--- app/assets/javascripts/utilities/countdown.js
+++ app/assets/javascripts/utilities/countdown.js
@@ -3,34 +3,29 @@ document.addEventListener('turbolinks:load', function() {
         return;
     }
 
-    let time_left = parseFloat(document.getElementById("countdown").dataset.timeleft);
-
-    // Set the date we're counting down to
-    let countDownDate = new Date();
-    countDownDate.setSeconds(countDownDate.getSeconds() + time_left);
-
     // Update the count down every 1 second
-    let x = setInterval(function() {
-        // Get todays date and time
-        let now = new Date().getTime();
+    let x = setInterval(function(){
+        let time_left_el = document.getElementById("countdown");
 
-        // Find the distance between now and the count down date
-        let distance = countDownDate - now;
+        // decrease time left value
+        let time_left = parseFloat(time_left_el.dataset.timeleft);
+        time_left = time_left - 1;
+        time_left_el.dataset.timeleft = time_left;
 
         // Time calculations for days, hours, minutes and seconds
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let days    = Math.floor(time_left / (60 * 60 * 24));
+        let hours   = Math.floor((time_left % (60 * 60 * 24)) / (60 * 60));
+        let minutes = Math.floor((time_left % (60 * 60)) / 60);
+        let seconds = Math.floor(time_left % 60);
 
         // Display the result in the element with id="countdown"
-        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+        time_left_el.innerHTML = days + "d " + hours + "h "
             + minutes + "m " + seconds + "s ";
 
         // If the count down is finished, write some text
-        if (distance <= 0) {
+        if (time_left <= 0) {
             clearInterval(x);
-            document.getElementById("countdown").textContent = "EXPIRED";
+            time_left_el.textContent = "EXPIRED";
             document.getElementsByTagName("form")[0].submit();
         }
     }, 1000);
